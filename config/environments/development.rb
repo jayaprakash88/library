27c631d8ca4630fd6d1d59c90ef9c15a0f65d907
Jayaprakash <prakash.assi@gmail.com>
2015-04-01 10:38:44 +0530

    Mail configuration for reset password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,11 +14,11 @@ First::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3008 }
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
@@ -34,4 +34,17 @@ First::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+
+  config.action_mailer.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+       :enable_starttls_auto => true,
+       :address            => 'smtp.gmail.com',
+       :port               => 587,
+       :domain             => 'prakash.assi@gmail.com',
+       :authentication     => 'plain',
+       :user_name          => 'prakash.assi@gmail.com',
+       :password           => 'suresh@88!' 
+    }
 end
